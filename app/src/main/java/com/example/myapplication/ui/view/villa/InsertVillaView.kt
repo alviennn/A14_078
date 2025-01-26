0cde92ef5985d6ebf1369f9dd95b0379b193c3ac
alviennn <alvien.rnp@gmail.com>
2025-01-27 01:23:03 +0700

    Menambahkan tampilan untuk memasukkan data villa baru dengan fitur validasi, input form, dan notifikasi setelah data berhasil disimpan

---
+++ app/src/main/java/com/example/myapplication/ui/view/villa/InsertVillaView.kt
@@ -0,0 +1,161 @@
+package com.example.myapplication.ui.view
+
+import CustomTopAppBar
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myapplication.ui.navigation.DestinasiNavigasi
+import com.example.myapplication.ui.viewmodel.PenyediaViewModel
+import com.example.myapplication.ui.viewmodel.villa.InsertVillaUiEvent
+import com.example.myapplication.ui.viewmodel.villa.InsertVillaUiState
+import com.example.myapplication.ui.viewmodel.villa.InsertVillaViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiInsertVilla : DestinasiNavigasi {
+    override val route = "insert_villa"
+    override val titleRes = "Insert Villa"
+    val descriptionRes = "Masukkan data Villa"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InsertVillaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertVillaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                modifier = modifier.fillMaxWidth(),
+                title = DestinasiInsertVilla.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                description = DestinasiInsertVilla.descriptionRes,
+                navigateUp = navigateBack
+            )
+        },
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { innerPadding ->
+        EntryVillaBody(
+            insertUiState = viewModel.uiVillaState,
+            onVillaValueChange = viewModel::updateInsertVillaState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertVilla()
+                    snackbarHostState.showSnackbar("Data Villa berhasil disimpan!")
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+
+@Composable
+fun EntryVillaBody(
+    insertUiState: InsertVillaUiState,
+    onVillaValueChange: (InsertVillaUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column (
+        modifier = modifier.padding(12.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        FormInputVilla(
+            insertUiEvent = insertUiState.insertVillaUiEvent,
+            onValueChange = onVillaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF2196F3),
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            )
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@Composable
+fun FormInputVilla(
+    insertUiEvent: InsertVillaUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertVillaUiEvent) -> Unit = {},
+    enabled: Boolean = true
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertUiEvent.namaVilla,
+            onValueChange = { onValueChange(insertUiEvent.copy(namaVilla = it)) },
+            label = { Text(text = "Nama Villa") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.alamat,
+            onValueChange = { onValueChange(insertUiEvent.copy(alamat = it)) },
+            label = { Text(text = "Alamat") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.kamarTersedia.toString(),
+            onValueChange = { onValueChange(insertUiEvent.copy(kamarTersedia = it.toIntOrNull() ?: 0)) },
+            label = { Text(text = "Kamar Tersedia") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled){
+            Text(
+                text = "Semua data harus terisi!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
